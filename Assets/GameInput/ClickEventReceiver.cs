abd1373cff6de569df851deb2bfba27532d826bc
SufAlex <50491112+SufAlex@users.noreply.github.com>
2022-09-06 19:54:32 +0300

    connected PlayerInput to custom script
    - created ClickEventReceiver
    - PlayerInput component settnigs changed
    - Position input added to .inputactions
    - small cleanup in RayCasterScript

---
+++ Assets/GameInput/ClickEventReceiver.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameInput
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class ClickEventReceiver : MonoBehaviour
+    {
+        private Vector2 _userPointerPosition;
+        
+        [UsedImplicitly]
+        public void OnClick(InputAction.CallbackContext clickEventContext)
+        {
+            switch (clickEventContext.phase)
+            {
+                case InputActionPhase.Performed:
+                    Debug.Log($"[Position] {_userPointerPosition}");
+                    break;
+            }
+        }
+        
+        [UsedImplicitly]
+        public void OnPosition(InputAction.CallbackContext pointerPositionContext)
+        {
+            switch (pointerPositionContext.phase)
+            {
+                case InputActionPhase.Performed:
+                    _userPointerPosition = pointerPositionContext.ReadValue<Vector2>();
+                    break;
+            }
+        }
+        
+    }
+}
--- Assets/Scripts/RayCasterScript.cs
+++ Assets/Scripts/RayCasterScript.cs
@@ -6,37 +6,27 @@ using UnityEngine.EventSystems;
 public class RayCasterScript : MonoBehaviour
 {
     
-    //[SerializeField] private Camera camera;
     [SerializeField] private AudioSource clickSong;
     
     private RaycastHit hit;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-//gameObject.camera
         Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
         
         if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0) 
                                           && !EventSystem.current.IsPointerOverGameObject()) {
-
             if (hit.transform.gameObject.GetComponent<IClickable>() != null)
             {
-                //Debug.Log("CLICK!!!");
                 hit.transform.gameObject.GetComponent<IClickable>().Click();
                 clickSong.Play();
             }
-
-
+            
             if(hit.transform.gameObject.tag == "Enemy")
                 Destroy(hit.transform.gameObject);
-            // Do something with the object that was hit by the raycast.
+
         }
     }
+    
+    
 }
